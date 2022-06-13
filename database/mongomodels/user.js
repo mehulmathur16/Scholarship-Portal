c0010dd23bcf1926016c51233d747fd2b9961dc3
mehulmathur16 <mathur.mehul01@gmail.com>
2022-06-13 15:07:48 +0530

    initial commit

---
+++ database/mongomodels/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mini-project', { useNewUrlParser: true });
+var passportLocalMongoose = require('passport-local-mongoose');
+var UserSchema = new mongoose.Schema({
+    password: String,
+    fullname: String,
+    username: String,  //email
+    category: String,
+    spass10: String,
+    spass12: String,
+    spassgrad: String,
+    state: String,
+    branch: String,
+    income: Number
+
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
+
