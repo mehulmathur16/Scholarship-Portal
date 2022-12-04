2b0cc933c22a116050cd751d725958429306dfa6
= <mathur.mehul01@gmail.com>
2022-12-04 15:22:36 +0530

    added axios deployed baseURL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://scholarship-portal.onrender.com/",
 })
 
 export default instance;
