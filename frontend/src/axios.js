e23eecb6d047c31346bd59d0578cbd8e86d5f531
= <mathur.mehul01@gmail.com>
2022-12-05 00:41:11 +0530

    changed axios baseURL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'https://scholarship-portal.onrender.com/';
+axios.defaults.baseURL = 'https://scholarship-portal-app.onrender.com/';
 
 // const instance = axios.create({
 //     baseURL: "https://scholarship-portal.onrender.com/",
