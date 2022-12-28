dccaa67d08832bc1954378202c2a83e3a97bddd8
= <mathur.mehul01@gmail.com>
2022-12-28 23:22:51 +0530

    changed region on render and redeployed backend

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'https://scholarship-portal-app.onrender.com/';
+axios.defaults.baseURL = 'https://scholarship-portal-backend.onrender.com/';
 
-// const instance = axios.create({
-//     baseURL: "https://scholarship-portal.onrender.com/",
-// })
 
 export default axios;
